d62b586c56254ccba435df6a27ef8020ce6e7c62
Jefferson Pedro <jeffersonpedro05@gmail.com>
2023-03-27 21:43:41 -0300

    update
    Refatoração de Codigo;
    Criação de Navegação no Routing Module;

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatCardModule} from '@angular/material/card';
+
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { FooterComponent } from './components/templates/footer/footer.component';
+import { HeaderComponent } from './components/templates/header/header.component';
+import { NavComponent } from './components/templates/nav/nav.component';
+import { HomeComponent } from './view/home/home.component';
+import { ProfileComponent } from './view/profile/profile.component';
+import { AboutComponent } from './view/about/about.component';
+import { SupportUsComponent } from './view/support-us/support-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    NavComponent,
+    HomeComponent,
+    ProfileComponent,
+    AboutComponent,
+    SupportUsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
