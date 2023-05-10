3bfde6af1c815dbaf59aecc4619fbb9028f2e9f8
Jefferson Pedro <jeffersonpedro05@gmail.com>
2023-05-10 00:15:29 -0300

    update
    Inclusão BackEnd Java

---
+++ backend/src/main/java/br/com/nutrinotes/project/NutriNotesApplication.java
@@ -0,0 +1,13 @@
+package br.com.nutrinotes.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutriNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutriNotesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/nutrinotes/project/NutriNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nutrinotes.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutriNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
