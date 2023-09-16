5809b380e5d5e662957ea2499a00ea4e8bebdb1b
Jefferson Pedro <jeffersonpedro05@gmail.com>
2023-09-16 16:50:58 -0300

    Revert "update"
    This reverts commit a3bb19a09e181e7812daf11533ffceae8fe12fe2.

--- backend/src/main/java/br/com/nutrinotes/controller/ChecklistController.java
+++ backend/src/main/java/br/com/nutrinotes/controller/ChecklistController.java
@@ -4,17 +4,21 @@ import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.nutrinotes.model.business.Business;
 import br.com.nutrinotes.model.checklist.Checklist;
 import br.com.nutrinotes.service.checklist.ICheckList;
 
@@ -26,7 +30,17 @@ public class ChecklistController {
 	@Autowired
 	ICheckList service;
 	
-	@GetMapping("all")
+	@GetMapping()
+	public ResponseEntity<Page<Checklist>> findAllPage(Pageable pageable){
+	    Page<Checklist> page = service.findAllPage(pageable);
+	    
+	    if(page.hasContent()) {
+	        return ResponseEntity.ok(page);
+	    }
+	    return ResponseEntity.notFound().build();
+	}
+	
+	@GetMapping("/all")
 	public ResponseEntity<List<Checklist>> findAll() {
 		List<Checklist> list = service.findAll();
 		if(list.size() > 0) {
@@ -62,10 +76,20 @@ public class ChecklistController {
 		return ResponseEntity.notFound().build();
 	}
 	
+	@PutMapping("/edit/{id}")
+	public ResponseEntity<Checklist> update(@RequestBody Checklist checklist, @PathVariable Integer id){
+		Checklist res = service.update(checklist, id);
+		if(res != null) {
+			return ResponseEntity.ok(res);
+		}
+		return ResponseEntity.badRequest().build();
+	}
+	
 	@DeleteMapping("/{id}")
 	public ResponseEntity<Checklist> delete(@PathVariable Integer id){
 		Checklist res = service.findById(id);
 		if(res != null) {
+			service.delete(id);
 			return ResponseEntity.noContent().build();
 		}
 		return ResponseEntity.notFound().build();
