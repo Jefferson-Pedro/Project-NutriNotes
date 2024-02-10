d10ff74f003cb986fc0fcfeb82494cc0804ec4e1
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-02-10 17:18:33 -0300

    Update NutriFilter.java
    -Alteração do nome da classe filter;

---
+++ backend/src/main/java/br/com/nutrinotes/security/NutriFilter.java
@@ -0,0 +1,37 @@
+package br.com.nutrinotes.security;
+
+import java.io.IOException;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class NutriFilter extends OncePerRequestFilter {
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+
+		if (request.getHeader("Authorization") != null) {
+
+			Authentication auth = TokenUtil.decode(request);
+
+			if (auth != null) {
+				SecurityContextHolder.getContext().setAuthentication(auth);
+
+			} else {
+				response.setStatus(401);
+				response.getWriter().println("Token invalido!");
+				return;
+			}
+		} 
+		// Encaminhando a requisição para frente
+		filterChain.doFilter(request, response);
+	}
+
+}
