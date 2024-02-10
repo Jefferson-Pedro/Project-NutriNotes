abc6bb66e5383bbe320771c2478b2d9e4a74124e
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-02-10 10:28:28 -0300

    Update reminder.ts

--- frontend/src/app/core/models/reminder.ts
+++ frontend/src/app/core/models/reminder.ts
@@ -1,9 +1,10 @@
 import { Profile } from "./Profile";
 
 export interface Reminder {
-    idReminder: number;
-    titulo: string,
-    descricao: string;
-    dataEvento: Date;
-    idProfile: Profile;
-  }
+  idReminder: number;
+  titulo: string;
+  descricao: string;
+  dataEvento: Date;
+  categoria: string;
+  idProfile: Profile; 
+}
