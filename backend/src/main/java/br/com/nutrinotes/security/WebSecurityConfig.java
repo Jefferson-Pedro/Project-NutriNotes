5c85ef8c816c20b05875b697e30d16acaf4c10f3
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-02-10 17:20:07 -0300

    Update WebSecurityConfig.java
    - Refatoração da classe de Segurança do Spring;

--- backend/src/main/java/br/com/nutrinotes/security/WebSecurityConfig.java
+++ backend/src/main/java/br/com/nutrinotes/security/WebSecurityConfig.java
@@ -1,12 +1,17 @@
 package br.com.nutrinotes.security;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -15,27 +20,58 @@ public class WebSecurityConfig {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		
-		http.csrf().disable()
-		 .authorizeHttpRequests()
-		 .requestMatchers(HttpMethod.POST, "/user", "/login").permitAll()
-		 
-		 .requestMatchers(HttpMethod.POST, "/business/new", "/checklist/new", "/profile/**", 
-				 							"question/**", "/item/**", "template/**", "/reminder/**").permitAll()
-		 
-		 .requestMatchers(HttpMethod.GET, "/profile/**", "/business/**", "/checklist/**", 
-				 						  "/reminder/**", "/item/**", "question/**", "template/**", "/department/**" ).permitAll()
-		 
-		 .requestMatchers(HttpMethod.DELETE, "/business/{id}", "/checklist/{id}", "question/{id}", 
-				 							"/item/delete/", "template/{id}"  ).permitAll()
-		 
-		 .requestMatchers(HttpMethod.PUT, "/business/edit/{id}", "/checklist/edit/{id}", 
-				 						 "question/edit/{id}","/item/edit/", "template/edit/{id}"  ).permitAll()
-	
-		 .anyRequest().authenticated().and().cors();
-
-		http.addFilterBefore(new MyFilter(), UsernamePasswordAuthenticationFilter.class);
+		http.cors(cors -> {
+			cors.configurationSource(this.corsConfigurationSource());
+		})
+		
+		//Desabilita a tela de login do SpringBoot
+		.csrf( (csrf) -> { 
+			csrf.disable();
+		})
+		
+		.authorizeHttpRequests( (auth) -> {
+			auth.requestMatchers(new  AntPathRequestMatcher("/user/new", "POST")).permitAll()
+			.requestMatchers(new  AntPathRequestMatcher("/login", "POST")).permitAll()
+			.requestMatchers(new  AntPathRequestMatcher("/business/new", "POST")).permitAll()
+			.requestMatchers(new  AntPathRequestMatcher("/checklist/new", "POST")).permitAll()
+			.requestMatchers(new  AntPathRequestMatcher("/profile/**", "POST")).permitAll()
+			.requestMatchers(new  AntPathRequestMatcher("/question/**", "POST")).permitAll()
+			.requestMatchers(new  AntPathRequestMatcher("/item/**", "POST")).permitAll()
+			.requestMatchers(new  AntPathRequestMatcher("/template/**", "POST")).permitAll()
+			.requestMatchers(new  AntPathRequestMatcher("/reminder/**", "POST")).permitAll()
+			.requestMatchers(new  AntPathRequestMatcher("/profile/**", "GET")).permitAll()
+			.requestMatchers(new  AntPathRequestMatcher("/business/**", "GET")).permitAll()
+			.requestMatchers(new  AntPathRequestMatcher("/checklist/**", "GET")).permitAll()
+			.requestMatchers(new  AntPathRequestMatcher("/reminder/**", "GET")).permitAll()
+			.requestMatchers(new  AntPathRequestMatcher("/item/**", "GET")).permitAll()
+			.requestMatchers(new  AntPathRequestMatcher("/question/**", "GET")).permitAll()
+			.requestMatchers(new  AntPathRequestMatcher("/template/**", "GET")).permitAll()
+			.requestMatchers(new  AntPathRequestMatcher("/department/**", "GET")).permitAll()
+			.requestMatchers(new  AntPathRequestMatcher("/business/{id}", "DELETE")).permitAll()
+			.requestMatchers(new  AntPathRequestMatcher("/checklist/{id}", "DELETE")).permitAll()
+			.requestMatchers(new  AntPathRequestMatcher("/question/{id}", "DELETE")).permitAll()
+			.requestMatchers(new  AntPathRequestMatcher("/item/delete/", "DELETE")).permitAll()
+			.requestMatchers(new  AntPathRequestMatcher("/template/{id}", "DELETE")).permitAll()
+			.requestMatchers(new  AntPathRequestMatcher("/business/edit/{id}", "PUT")).permitAll()
+			.requestMatchers(new  AntPathRequestMatcher("/checklist/edit/{id}", "PUT")).permitAll()
+			.requestMatchers(new  AntPathRequestMatcher("/question/edit/{id}", "PUT")).permitAll()
+			.requestMatchers(new  AntPathRequestMatcher("/item/edit/", "PUT")).permitAll()
+			.requestMatchers(new  AntPathRequestMatcher("/template/edit/{id}", "PUT")).permitAll()
+		    .anyRequest().authenticated();
+	})
+		.addFilterBefore(new NutriFilter(), UsernamePasswordAuthenticationFilter.class)
+		.headers((header) -> header.frameOptions((iframe) -> iframe.disable()));
 		
 		return http.build();
-		
+	}
+	
+	public CorsConfigurationSource corsConfigurationSource() {
+	    CorsConfiguration configuration = new CorsConfiguration();
+	    configuration.setAllowedOrigins(Arrays.asList("*"));
+	    configuration.setAllowedMethods(Arrays.asList("*"));
+	    configuration.setAllowedHeaders(Arrays.asList("*"));
+	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    source.registerCorsConfiguration("/**", configuration);
+	    return source;
 	}
 }
