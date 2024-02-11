99cf5597c291d568b748f9a736ef74dc6b2515d3
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-02-11 13:23:44 -0300

    Update ITemplateChecklistService.java
    - Refatoração no nome do método;

--- backend/src/main/java/br/com/nutrinotes/service/templates/ITemplateChecklistService.java
+++ backend/src/main/java/br/com/nutrinotes/service/templates/ITemplateChecklistService.java
@@ -7,7 +7,7 @@ import br.com.nutrinotes.model.templates.TemplateChecklist;
 
 public interface ITemplateChecklistService {
 	
-	public TemplateChecklist save(TemplateChecklist newTemplate);
+	public TemplateChecklist create(TemplateChecklist newTemplate);
 	public boolean update(TemplateChecklist templateChecklist, Integer id);
 	public List<TemplateChecklist> findAll();
 	public List<TemplateChecklist> findByName(String nome);
