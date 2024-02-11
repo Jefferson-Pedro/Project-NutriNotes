25f06684333fdaecbedfc7776c2f6201c223a811
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-02-10 21:57:14 -0300

    Update UserDAO.java
    - Inclusão do metodo findByNomeContaining para busca de nome no banco de dados;

--- backend/src/main/java/br/com/nutrinotes/dao/user/UserDAO.java
+++ backend/src/main/java/br/com/nutrinotes/dao/user/UserDAO.java
@@ -1,9 +1,14 @@
 package br.com.nutrinotes.dao.user;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.nutrinotes.model.user.User;
 
+
 public interface UserDAO extends JpaRepository<User, Integer>{
+	
 	public User findByLogin(String login);
+	public List<User> findByNomeContaining(String palavraChave);
 }
