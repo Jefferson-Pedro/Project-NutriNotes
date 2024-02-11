49ab57ee39061a469a6cb55b6f02629903a1ffca
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-02-11 16:41:17 -0300

    Update IAuthService.java
    -Refatoração do método authenticate para receber uma DTO de Login;

--- backend/src/main/java/br/com/nutrinotes/service/auth/IAuthService.java
+++ backend/src/main/java/br/com/nutrinotes/service/auth/IAuthService.java
@@ -1,9 +1,10 @@
 package br.com.nutrinotes.service.auth;
 
 import br.com.nutrinotes.dto.AuthDTO;
+import br.com.nutrinotes.dto.LoginDTO;
 import br.com.nutrinotes.model.user.User;
 
 public interface IAuthService {
 	public User create(User newUser);
-	public AuthDTO authenticate(User dataLogin);
+	public AuthDTO authenticate(LoginDTO login);
 }
