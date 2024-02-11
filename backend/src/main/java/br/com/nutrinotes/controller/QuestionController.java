3399b667967bf069a03a36821b58d997fd96fa13
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-02-11 11:51:31 -0300

    Update QuestionController.java
    - Refatoração e inclusão das Constraints no controller;

--- backend/src/main/java/br/com/nutrinotes/controller/QuestionController.java
+++ backend/src/main/java/br/com/nutrinotes/controller/QuestionController.java
@@ -4,8 +4,8 @@ import java.net.URISyntaxException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,7 +20,11 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.nutrinotes.dto.QuestionDTO;
 import br.com.nutrinotes.model.questions.Questions;
 import br.com.nutrinotes.service.questions.IQuestions;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
+@Validated
 @RestController
 @CrossOrigin("*")
 @RequestMapping("/question")
@@ -39,7 +43,7 @@ public class QuestionController {
 	}
 	
 	@GetMapping("/{id}")
-	public ResponseEntity<Questions> findById(@PathVariable Integer id){
+	public ResponseEntity<Questions> findById(@PathVariable @NotNull @Positive Integer id){
 		Questions res = service.findById(id);
 		if(res != null) {
 			return ResponseEntity.ok().body(res);
@@ -48,7 +52,7 @@ public class QuestionController {
 	}
 	
 	@GetMapping("template/{id}")
-	public ResponseEntity<List<QuestionDTO>> findQuestionsByTemplate(@PathVariable Integer id){
+	public ResponseEntity<List<QuestionDTO>> findQuestionsByTemplate(@PathVariable @NotNull @Positive Integer id){
 		List<QuestionDTO> list = service.finQuestionsByTemplate(id);
 		if(!list.isEmpty()) {
 			return ResponseEntity.ok().body(list);
@@ -57,7 +61,7 @@ public class QuestionController {
 	}
 	
 	@GetMapping("/search")
-	public ResponseEntity<List<Questions>> findByName(@RequestParam (name = "questao") String name){
+	public ResponseEntity<List<Questions>> findByName(@RequestParam (name = "questao") @NotNull String name){
 		List<Questions> list = service.findByQuestions(name);
 		if(!list.isEmpty()) {
 			return ResponseEntity.ok(list);
@@ -66,31 +70,29 @@ public class QuestionController {
 	}
 	
 	@PostMapping("/new")
-	public ResponseEntity<Questions> save(@RequestBody Questions newQuestion) throws URISyntaxException{
+	public ResponseEntity<Questions> create(@RequestBody @NotNull @Valid Questions newQuestion) throws URISyntaxException{
 		
-		if (newQuestion == null) {	
-			 return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-		}
-		if (service.save(newQuestion)) {
+		if (service.create(newQuestion) != null) {
 			return ResponseEntity.ok(newQuestion);
 		}
 		return ResponseEntity.badRequest().build();
 	}
 	
 	@PutMapping("/edit/{id}")
-	public ResponseEntity<Questions> update(@RequestBody Questions Question, @PathVariable Integer id){
-		Questions res = service.update(Question, id);
-		if(res != null) {
+	public ResponseEntity<Questions> update(@RequestBody @NotNull @Valid Questions Question, 
+											@PathVariable @NotNull @Positive Integer id){
+		
+		if(service.update(Question, id)) {
+			Questions res = service.findById(id);
 			return ResponseEntity.ok(res);
 		}
 		return ResponseEntity.badRequest().build();
 	}
 	
 	@DeleteMapping("/{id}")
-	public ResponseEntity<Questions> delete(@PathVariable Integer id) {
-		Questions res = service.findById(id);
-		if(res != null) { 
-			service.delete(id);
+	public ResponseEntity<Questions> delete(@PathVariable @NotNull @Positive Integer id) {
+		
+		if(service.delete(id)) { 
 			return ResponseEntity.noContent().build();
 		}
 		return ResponseEntity.notFound().build();
