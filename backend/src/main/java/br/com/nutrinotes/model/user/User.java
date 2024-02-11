afaaebb8add3c2a6527ce3b3b8a2c8a9c860e5f6
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-02-10 21:55:38 -0300

    Update User.java
    - refatoração e inclusão das constraints na classe User;

--- backend/src/main/java/br/com/nutrinotes/model/user/User.java
+++ backend/src/main/java/br/com/nutrinotes/model/user/User.java
@@ -1,38 +1,124 @@
 package br.com.nutrinotes.model.user;
 
-import br.com.nutrinotes.model.profile.Profile;
+import java.time.LocalDate;
+import java.util.List;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import br.com.nutrinotes.model.business.Business;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
-@Table(name = "user")
+@Table(name="user")
 public class User {
 	
 	@Id
-	@Column(name="login", length = 50)
-	private String login;
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id_profile")
+	private Integer idProfile;
 	
-	@Column(name="senha", length = 50)
+	@NotBlank
+	@NotNull(message = "O campo nome não pode ser nulo")
+	@Length(min = 3, max = 75)
+	@Column(name="nome", length = 100)
+	private String nome;
+	
+	@NotBlank
+	@NotNull(message = "O campo Data de Nascimento não pode ser nulo")
+	@Length(min = 3, max = 75)
+	@Column(name="data_nasc")
+	private LocalDate data_nasc;
+	
+	@NotBlank
+	@NotNull(message = "O campo sexo não pode ser nulo")
+	@Length(min = 3, max = 75)
+	@Column(name="sexo", length = 10)
+	private String sexo;
+	
+	@NotBlank
+	@Email(message = "Email não é válido")
+	@NotNull(message = "O campo nome não pode ser nulo")
+	@Length(min = 3, max = 75)
+	@Column(name="email", length = 50)
+	private String email;
+	
+	@NotBlank
+	@NotNull(message = "O campo senha não pode ser nulo")
+	@Length(min = 3, max = 45)
+	@Column(name="senha", length = 45)
 	private String senha;
 	
-	@ManyToOne
-	@JoinColumn(name="id_profile")
-	private Profile idProfile;
+	@NotBlank
+	@NotNull(message = "O campo senha não pode ser nulo")
+	@Length(min = 3, max = 45)
+	@Column(name="telefone", length = 45)
+	private String telefone;
 	
-	//GET E SETS
-
-	public String getLogin() {
-		return login;
+	@NotBlank
+	@NotNull(message = "O campo sexo não pode ser nulo")
+	@Length(min = 3, max = 75)
+	@Column(name="crn", length = 45)
+	private String crn;
+	
+	
+	@OneToMany(mappedBy = "responsavelTec", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("responsavelTec")
+	private List<Business> business;
+		
+	//GETS E SETS
+	
+	public Integer getIdProfile() {
+		return idProfile;
 	}
 
-	public void setLogin(String login) {
-		this.login = login;
+	public void setIdProfile(Integer idProfile) {
+		this.idProfile = idProfile;
 	}
 
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public LocalDate getData_nasc() {
+		return data_nasc;
+	}
+
+	public void setData_nasc(LocalDate data_nasc) {
+		this.data_nasc = data_nasc;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
 	public String getSenha() {
 		return senha;
 	}
@@ -41,11 +127,33 @@ public class User {
 		this.senha = senha;
 	}
 
-	public Profile getIdProfile() {
-		return idProfile;
+	public String getTelefone() {
+		return telefone;
 	}
 
-	public void setIdProfile(Profile idProfile) {
-		this.idProfile = idProfile;
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getCrn() {
+		return crn;
+	}
+
+	public void setCrn(String crn) {
+		this.crn = crn;
+	}
+	
+	public List<Business> getBusiness() {
+		return business;
+	}
+
+	public void setBusiness(List<Business> business) {
+		this.business = business;
+	}
+
+	@Override
+	public String toString() {
+		return "Profile [idProfile=" + idProfile + ", nome=" + nome + ", data_nasc=" + data_nasc + ", sexo=" + sexo
+				+ ", email=" + email + ", senha=" + senha + ", telefone=" + telefone + ", crn=" + crn + ", business=" + business + "]";
 	}
 }
