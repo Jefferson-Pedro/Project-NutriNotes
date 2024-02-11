84777d663e35c8af8a0597b096c43b7eb886f14a
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-02-10 23:48:14 -0300

    Update BusinessDAO.java

--- backend/src/main/java/br/com/nutrinotes/dao/business/BusinessDAO.java
+++ backend/src/main/java/br/com/nutrinotes/dao/business/BusinessDAO.java
@@ -3,11 +3,9 @@ package br.com.nutrinotes.dao.business;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.nutrinotes.model.business.Business;
-import br.com.nutrinotes.model.department.Department;
 
 @Repository
 public interface BusinessDAO extends JpaRepository<Business, Integer>{
