c092bd566cf8d78c831c16b19323d16be8324402
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-02-10 23:59:07 -0300

    Update IUser.java

--- backend/src/main/java/br/com/nutrinotes/service/user/IUser.java
+++ backend/src/main/java/br/com/nutrinotes/service/user/IUser.java
@@ -3,10 +3,12 @@ package br.com.nutrinotes.service.user;
 import java.util.List;
 
 import br.com.nutrinotes.model.user.User;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 
 public interface IUser {
 	
-	public User save(User novo);
+	public User create(@Valid @NotNull User novo);
 	public boolean update(User user, Integer id);
 	public List<User> findAll();
 	public List<User> findByName(String nome);
