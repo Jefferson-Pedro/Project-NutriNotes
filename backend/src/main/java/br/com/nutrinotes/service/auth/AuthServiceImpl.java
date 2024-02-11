10067e98227aca4b1802755ffe3ace4301bd0bdb
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-02-10 23:48:47 -0300

    Update AuthServiceImpl.java
    - Inclusão das constraints nos métodos da classe AuthServiceImpl ;

--- backend/src/main/java/br/com/nutrinotes/service/auth/AuthServiceImpl.java
+++ backend/src/main/java/br/com/nutrinotes/service/auth/AuthServiceImpl.java
@@ -2,22 +2,26 @@ package br.com.nutrinotes.service.auth;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
 
 import br.com.nutrinotes.dao.user.UserDAO;
 import br.com.nutrinotes.dto.AuthDTO;
 import br.com.nutrinotes.model.user.User;
 import br.com.nutrinotes.security.NutriToken;
 import br.com.nutrinotes.security.TokenUtil;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 
-@Component
+@Validated
+@Service
 public class AuthServiceImpl implements IAuthService {
 	
 	@Autowired
 	UserDAO dao;
 
 	@Override
-	public User createUser(User newUser) {
+	public User createUser(@Valid @NotNull User newUser) {
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(10);
 		String newPassword = encoder.encode(newUser.getSenha());
 		newUser.setSenha(newPassword);
@@ -25,14 +29,14 @@ public class AuthServiceImpl implements IAuthService {
 	}
 
 	@Override
-	public AuthDTO authenticate(User dataLogin) {
-		User res = dao.findByLogin(dataLogin.getLogin());
+	public AuthDTO authenticate(@Valid @NotNull User dataLogin) {
+		User res = dao.findByLogin(dataLogin.getEmail());
 		
 		if(res != null) {
 			BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 			if(encoder.matches(dataLogin.getSenha(), res.getSenha())) {
 				NutriToken token = TokenUtil.encode(res);
-				return new AuthDTO(res.getIdProfile(),res.getLogin(),token.toString());
+				return new AuthDTO(res.getIdProfile(),res.getEmail(),token.toString());
 			}
 		}
 		return null;
