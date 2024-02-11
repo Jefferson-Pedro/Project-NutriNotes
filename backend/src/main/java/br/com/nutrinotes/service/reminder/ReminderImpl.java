315d09ab64ee698e6cf325ed0c8751e41ee9306f
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-02-10 23:28:40 -0300

    Update ReminderImpl.java
    - Inclusão das constraints nos métodos da classe ReminderImpl;

--- backend/src/main/java/br/com/nutrinotes/service/reminder/ReminderImpl.java
+++ backend/src/main/java/br/com/nutrinotes/service/reminder/ReminderImpl.java
@@ -3,11 +3,11 @@ package br.com.nutrinotes.service.reminder;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
@@ -17,6 +17,7 @@ import br.com.nutrinotes.dto.ReminderMapper;
 import br.com.nutrinotes.model.reminder.Reminder;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 @Validated
 @Service
@@ -36,11 +37,16 @@ public class ReminderImpl implements IReminderService{
 	}
 
 	@Override
-	public Reminder update(Reminder reminder) {
-		if(reminder !=null) {
-			return dao.save(reminder);
-		}
-		return null;
+	public boolean update(@Valid @NotNull Reminder reminder, @NotNull @Positive Integer id) {
+		Optional<Reminder> res = dao.findById(id);
+	    if (res.isPresent()) {
+	    	Reminder existingBusiness = res.get();
+	        BeanUtils.copyProperties(reminder, existingBusiness, "idReminder");
+	        dao.save(existingBusiness);
+	        return true;
+	    }
+	    System.err.println("Erro ao editar o evento!");
+	    return false;
 	}
 	
 	@Override
@@ -49,12 +55,12 @@ public class ReminderImpl implements IReminderService{
 	}
 
 	@Override
-	public Reminder findById(Integer id) {
+	public Reminder findById(@NotNull @Positive Integer id) {
 		return dao.findById(id).orElse(null);
 	}
 
 	@Override
-	public boolean delete(Integer id) {
+	public boolean delete(@NotNull @Positive Integer id) {
 		Optional<Reminder> reminder = dao.findById(id);
 		if(reminder != null) {
 			dao.deleteById(id);
