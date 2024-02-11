2bd0f414cc650bcbab3826e0aab1ec96ba964e95
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-02-10 23:58:23 -0300

    Update AuthController.java
    -Refatoração do metodo create e inclusão das constraints nos métodos da classe TemplateChecklistImpl;

--- backend/src/main/java/br/com/nutrinotes/controller/AuthController.java
+++ backend/src/main/java/br/com/nutrinotes/controller/AuthController.java
@@ -2,15 +2,19 @@ package br.com.nutrinotes.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.nutrinotes.dto.AuthDTO;
 import br.com.nutrinotes.model.user.User;
-import br.com.nutrinotes.security.NutriToken;
 import br.com.nutrinotes.service.auth.IAuthService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 
+@Validated
 @RestController
 @CrossOrigin("*")
 public class AuthController {
@@ -18,22 +22,25 @@ public class AuthController {
 	@Autowired
 	private IAuthService service;
 	
+
 	@PostMapping("/user")
-	public ResponseEntity<User> createUser(@RequestBody User user){
-		User res = service.createUser(user);
-		if(res != null) {
-			return ResponseEntity.ok(res);
+	public ResponseEntity<?> create(@RequestBody @Valid @NotNull User user){
+		
+		if(service.create(user) != null) {
+			return ResponseEntity.ok().body("Usuário criado com sucesso!");
 		}
-		return ResponseEntity.badRequest().build();
+		return ResponseEntity.status(403).body("ERRO! Usuário já existe no Banco de Dados!");
 	}
 	
 	@PostMapping("/login")
-	public ResponseEntity<NutriToken> authenticate(@RequestBody User dataUser){
-		NutriToken token = service.authenticate(dataUser);
-		if(token != null) {
-			return ResponseEntity.ok(token);
+	public ResponseEntity<AuthDTO> authenticate(@RequestBody @Valid @NotNull User dataUser){
+		
+		AuthDTO auth = service.authenticate(dataUser);
+		
+		if(auth != null) {
+			return ResponseEntity.ok(auth);
 		}
-		return ResponseEntity.status(403).build();
+		return ResponseEntity.status(401).build();
 	}
 	
 }
