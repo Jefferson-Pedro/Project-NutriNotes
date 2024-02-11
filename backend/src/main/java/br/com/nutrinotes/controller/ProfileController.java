33493a9db17d9fcc7c9e23b4af93a69c7faf2d05
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-02-10 21:56:40 -0300

    Update ProfileController.java
    - Refatoração;

--- backend/src/main/java/br/com/nutrinotes/controller/ProfileController.java
+++ backend/src/main/java/br/com/nutrinotes/controller/ProfileController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.nutrinotes.model.profile.Profile;
+import br.com.nutrinotes.model.user.Profile;
 import br.com.nutrinotes.service.profile.IProfile;
 
 @RestController
