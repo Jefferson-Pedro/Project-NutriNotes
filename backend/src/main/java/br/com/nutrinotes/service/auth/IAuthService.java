705caad9ae5ee029effa79fa17bca29bc22a8eae
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-02-10 21:59:10 -0300

    Update IAuthService.java

--- backend/src/main/java/br/com/nutrinotes/service/auth/IAuthService.java
+++ backend/src/main/java/br/com/nutrinotes/service/auth/IAuthService.java
@@ -1,9 +1,9 @@
 package br.com.nutrinotes.service.auth;
 
+import br.com.nutrinotes.dto.AuthDTO;
 import br.com.nutrinotes.model.user.User;
-import br.com.nutrinotes.security.NutriToken;
 
 public interface IAuthService {
 	public User createUser(User newUser);
-	public NutriToken authenticate(User dataLogin);
+	public AuthDTO authenticate(User dataLogin);
 }
