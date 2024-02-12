3504abd257f6aa5b64c877210ba180f4d4122254
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-02-11 22:38:30 -0300

    Update IUser.java
    - Refatoração e inclusão das regras de restrição;

--- backend/src/main/java/br/com/nutrinotes/service/user/IUser.java
+++ backend/src/main/java/br/com/nutrinotes/service/user/IUser.java
@@ -4,14 +4,16 @@ import java.util.List;
 
 import br.com.nutrinotes.model.user.User;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 public interface IUser {
 	
 	public User create(@Valid @NotNull User novo);
-	public boolean update(User user, Integer id);
+	public boolean update(@Valid @NotNull User User, @NotNull @Positive Integer id);
 	public List<User> findAll();
-	public List<User> findByName(String nome);
-	public User findById(Integer id);
-	public boolean delete(Integer id);
+	public List<User> findByName(@NotNull @NotBlank String nome);
+	public User findById(@NotNull @Positive Integer id);
+	public boolean delete(@NotNull @Positive Integer id);
 }
