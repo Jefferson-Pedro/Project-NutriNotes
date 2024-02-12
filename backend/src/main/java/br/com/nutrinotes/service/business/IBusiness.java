a4849845eaf9e635fe8504eaad83c9466190de00
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-02-12 16:40:27 -0300

    Update IBusiness.java
    - Inclusão das contraints nos métodos da interface IBusiness;
    - Mudança no retorno do método FindAll;

--- backend/src/main/java/br/com/nutrinotes/service/business/IBusiness.java
+++ backend/src/main/java/br/com/nutrinotes/service/business/IBusiness.java
@@ -5,15 +5,20 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import br.com.nutrinotes.dto.BusinessDTO;
 import br.com.nutrinotes.model.business.Business;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 public interface IBusiness {
 	
-	public Business create(Business novo);
-	public boolean update(Business business, Integer id);
+	public Business create(@Valid @NotNull Business novo);
+	public boolean update(@Valid @NotNull Business business, @NotNull @Positive Integer id);
 	public Page<Business> findAllPage(Pageable pageable);
-	public List<Business> findAll();
-	public List<Business> findByName(String nome);
-	public Business findById(Integer id);
-	public boolean delete(Integer id);
+	public List<BusinessDTO> findAll();
+	public List<Business> findByName(@NotBlank @NotNull String nome);
+	public Business findById(@NotNull @Positive  Integer id);
+	public boolean delete(@NotNull @Positive Integer id);
 }
