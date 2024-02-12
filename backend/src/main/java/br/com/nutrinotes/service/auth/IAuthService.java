6025f3e2f38a1439206c1721fb06978152363294
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-02-11 22:38:04 -0300

    Update IAuthService.java

--- backend/src/main/java/br/com/nutrinotes/service/auth/IAuthService.java
+++ backend/src/main/java/br/com/nutrinotes/service/auth/IAuthService.java
@@ -2,9 +2,10 @@ package br.com.nutrinotes.service.auth;
 
 import br.com.nutrinotes.dto.AuthDTO;
 import br.com.nutrinotes.dto.LoginDTO;
+import jakarta.validation.constraints.NotNull;
 
 
 public interface IAuthService {
 	
-	public AuthDTO authenticate(LoginDTO login);
+	public AuthDTO authenticate(@NotNull LoginDTO login);
 }
