fc770febf656182335e825eb0f7f19e92af68fb9
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-02-14 13:49:07 -0300

    Update IUser.java
    - Mudança no retorno do método findAll de User > UserDTO

--- backend/src/main/java/br/com/nutrinotes/service/user/IUser.java
+++ backend/src/main/java/br/com/nutrinotes/service/user/IUser.java
@@ -2,6 +2,7 @@ package br.com.nutrinotes.service.user;
 
 import java.util.List;
 
+import br.com.nutrinotes.dto.UserDTO;
 import br.com.nutrinotes.model.user.User;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
@@ -12,7 +13,7 @@ public interface IUser {
 	
 	public User create(@Valid @NotNull User novo);
 	public boolean update(@Valid @NotNull User User, @NotNull @Positive Integer id);
-	public List<User> findAll();
+	public List<UserDTO> findAll();
 	public List<User> findByName(@NotNull @NotBlank String nome);
 	public User findById(@NotNull @Positive Integer id);
 	public boolean delete(@NotNull @Positive Integer id);
