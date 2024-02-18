e302f9dcdd3553956a79125271f2860701b416ca
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-02-18 18:08:52 -0300

    Update IDepartment.java
    - Inclusão das Constraints no metodos da interface IDepartment ;

--- backend/src/main/java/br/com/nutrinotes/service/department/IDepartment.java
+++ backend/src/main/java/br/com/nutrinotes/service/department/IDepartment.java
@@ -6,16 +6,20 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import br.com.nutrinotes.model.department.Department;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 public interface IDepartment {
 	
-	public Department create(Department novo);
-	public boolean update(Department department, Integer id);
+	public Department create(@Valid @NotNull Department novo);
+	public boolean update(@Valid @NotNull Department department, @NotNull @Positive Integer id);
 	public Page<Department> findAllPage(Pageable pageable);
 	public List<Department> findAll();
-	public List<Department> findByName(String nome);
-	public Department findById(Integer id);
-	public boolean delete(Integer id);
-	public List<Department> findDepartmentByIdBusiness(Integer id);
+	public List<Department> findByName(@NotNull @NotBlank String nome);
+	public Department findById(@NotNull @Positive Integer id);
+	public boolean delete(@NotNull @Positive Integer id);
+	public List<Department> findDepartmentByIdBusiness(@NotNull @Positive Integer id);
 	
 }
