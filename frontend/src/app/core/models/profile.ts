3b82cab3f1c723ce308402e34ee88b2289b96c52
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-02-18 19:00:11 -0300

    Delete profile.ts

--- frontend/src/app/core/models/profile.ts
+++
@@ -1,9 +0,0 @@
-export interface Profile  {
-    idProfile?: number,
-    nome: string,
-    data_nasc: Date | null,
-    sexo: string,
-    email: string,
-    telefone: string,
-    crn: string,
-}
