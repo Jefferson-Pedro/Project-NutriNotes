68ea05fec10f796a0ae328ae55b00b52b6ccee81
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-02-18 19:01:54 -0300

    Create user.service.ts
    - Criação do User Service;

---
+++ frontend/src/app/core/services/user/user.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { environment } from 'src/environments/environment.development';
+import { User } from '../../models/Users';
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  private URL_BASE = environment.baseUrl;
+
+  constructor(private http: HttpClient) {}
+
+  public create(user: User): Observable<User> {
+    const url = `${environment.baseUrl}/user/new`;
+
+    return this.http.post<User>(url, user);
+  }
+
+  public update(user: User): Observable<User> {
+    const url = `${environment.baseUrl}/user/edit/${user.idUser}`;
+
+    return this.http.put<User>(url, user);
+  }
+}
