114f5dc793a008007723c18d3cd1a967e8fc9ccb
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-02-23 16:43:57 -0300

    Create UserResponseDTO.java

---
+++ backend/src/main/java/br/com/nutrinotes/dto/UserResponseDTO.java
@@ -0,0 +1,70 @@
+package br.com.nutrinotes.dto;
+
+import java.util.List;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public class UserResponseDTO {
+	
+	private Integer idUser;
+	
+	@NotBlank
+	@NotNull(message = "O campo nome não pode ser nulo")
+	private String nome;
+			
+	@NotBlank
+	@Email(message = "Email não é válido")
+	@NotNull(message = "O campo nome não pode ser nulo")
+	private String email;
+	
+	@NotBlank
+	@NotNull(message = "O campo sexo não pode ser nulo")
+	private String crn;
+	
+	private List<BusinessDTO> business;
+
+	//GET AND SET
+	
+	public Integer getIdUser() {
+		return idUser;
+	}
+
+	public void setIdUser(Integer idUser) {
+		this.idUser = idUser;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCrn() {
+		return crn;
+	}
+
+	public void setCrn(String crn) {
+		this.crn = crn;
+	}
+
+	public List<BusinessDTO> getBusiness() {
+		return business;
+	}
+
+	public void setBusiness(List<BusinessDTO> business) {
+		this.business = business;
+	}
+}
