c211eff67ad4570cf7e183abc8a1a4253a53449c
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-03-19 01:32:44 -0300

    Update local-storage.service.ts
    - Mudança do nome das variaveis para nome semanticos

--- frontend/src/app/features/shared-module/services/localStorage/local-storage.service.ts
+++ frontend/src/app/features/shared-module/services/localStorage/local-storage.service.ts
@@ -8,28 +8,28 @@ import { AuthDTO } from 'src/app/core/models/AuthDTO';
 export class LocalStorageService {
 
    // variável que irá ser setada com o tipo AuthDTO quando logado e null quando deslogado
-   private isLogged$ = new BehaviorSubject<AuthDTO | null>(null);
+   private loggedInUserInfo$ = new BehaviorSubject<AuthDTO | null>(null);
 
    // variável que será usada para armazenar o valor da variável acima
-   public isLoggedInUser$ = this.isLogged$.asObservable();
+   public isLoggedInUser$ = this.loggedInUserInfo$.asObservable();
  
 
   constructor() { }
 
-  public saveLoggedInUserInfo(res: AuthDTO){
-    localStorage.setItem("NutriToken", JSON.stringify(res));
+  public saveLoggedInUserInfo(userInfo: AuthDTO){
+    localStorage.setItem("NutriToken", JSON.stringify(userInfo));
 
-    this.isLogged$.next(res);
+    this.loggedInUserInfo$.next(userInfo);
   }
 
   public getLoggedInUserInfo():string | null{
     return localStorage.getItem('NutriToken');
   }
 
-  public removeToken(): void{
+  public removeLoggedInUserInfo(): void{
     localStorage.removeItem('NutriToken');
     
-    this.isLogged$.next(null);
+    this.loggedInUserInfo$.next(null);
   }
 
 }
