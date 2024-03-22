c77dfc8d28faa123371724d4fbac2c4afe901e71
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-03-22 08:24:47 -0300

    Delete nutriToken.ts

--- frontend/src/app/core/utils/nutriToken.ts
+++
@@ -1,9 +0,0 @@
-import { jwtDecode } from 'jwt-decode';
-
-export const jwtDecoder = (token: string): any => {
-    try {
-        return jwtDecode(token);
-    } catch (error) {
-        return null;
-    }
-}
