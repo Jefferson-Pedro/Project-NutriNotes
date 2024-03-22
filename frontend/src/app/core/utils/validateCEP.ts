61e7c698636d73ff61436bebfd51362f717cc1ab
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-03-22 08:24:54 -0300

    Delete validateCEP.ts

--- frontend/src/app/core/utils/validateCEP.ts
+++
@@ -1,7 +0,0 @@
-import { removeSpecialCharacters } from './removeCepSpecialCaracthers';
-
-export const validateCEP = (cep: string): boolean => {
-  const regex = /^[0-9]{8}$/;
-
-  return regex.test(removeSpecialCharacters(cep));
-};
