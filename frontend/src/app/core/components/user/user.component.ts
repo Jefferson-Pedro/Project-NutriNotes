bc2e2015bae76c57162ec1c4a36a77a4b92dceec
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-03-23 14:01:43 -0300

    Update user.component.ts
    - Refatoração e inicio da construção do método Loaduser

--- frontend/src/app/core/components/user/user.component.ts
+++ frontend/src/app/core/components/user/user.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { NotificationService } from 'src/app/features/shared-module/services/notification';
 import { CreateUser } from '../../models/CreateUser';
 import { UserService } from '../../services/user';
+import { EditUser } from '../../models/EditUser';
+import { LocalStorageService } from 'src/app/features/shared-module/services/localStorage';
 
 
 @Component({
@@ -12,17 +14,19 @@ import { UserService } from '../../services/user';
   styleUrls: ['./user.component.css'],
 })
 export class UserComponent implements OnInit {
+  private formBuilder = inject(FormBuilder);
+  private route = inject(Router);
+  private userService = inject(UserService);
+  private notificationService = inject(NotificationService);
+  private localStorageService = inject(LocalStorageService);
+
   protected form = this.buildForm();
   protected loading!: boolean;
   protected submitted = false;
   private user?: CreateUser;
+ // private userLoggedin;
 
-  constructor(
-    private formBuilder: FormBuilder,
-    private route: Router,
-    private userService: UserService,
-    private notificationService: NotificationService
-  ) {}
+  constructor() {}
 
   ngOnInit(): void {
     this.loadUser();
@@ -36,13 +40,8 @@ export class UserComponent implements OnInit {
         'Preencha todos os campos corretamente!'
       );
       return; //Quebra a função e não executa mais nada.
-    }
-
-    if (this.user) {
-      this.editUser();
-      return;
-    }
-    this.createUser();
+    }    
+      this.editUser(); 
   }
 
   public onCancel() {
@@ -57,11 +56,13 @@ export class UserComponent implements OnInit {
       email: ['', [Validators.required, Validators.email]],
       telefone: [''],
       crn: ['', Validators.required],
+      senha: [''],
+      novaSenha: ['']
     });
   }
 
-  private createOrUpdateUserPayload(): CreateUser {
-    //Armazena o valor cru do forms
+  private updateUserPayload(): EditUser {
+      //Armazena o valor cru do forms
     const formValue = this.form.getRawValue();
     return {
       idUser: this.user?.idUser,
@@ -71,39 +72,35 @@ export class UserComponent implements OnInit {
       email: formValue.email,
       telefone: formValue.telefone,
       crn: formValue.crn,
+      senha: formValue.senha,
+      novaSenha: formValue.novaSenha
     };
-  }
-
-  private createUser(): void {
-    this.loading = true;
-    const body = this.createOrUpdateUserPayload();
-
-    this.userService.create(body).subscribe({
-      next: () => {
-        this.notificationService.showMessageSucess('Perfil salvo com sucesso!');
-      },
-      error: () => {
-        this.notificationService.showMessageFail(
-          'Ocorreu um erro ao salvar as informações do perfil'
-        );
-      },
-      complete: () => {
-        //quando completar requisição cai nessa função;
-        this.loading = false;
-      },
-    });
   }
 
   private loadUser(): void {
     this.loading = true;
     //Criar a parte da autenticação
+    this.localStorageService.loggedUser$.subscribe({
+      next: (userResponse) => {
+        const user = this.findByIdUser();
+      }, 
+      error: (err) => {
+          console.log("Nenhum usuário logado: ", err);
+      },
+      });
+
+
     // criar no back end função que retorna profile se existir
     // criar função no serviço para consumir o endpoint
     //Se um usuário existir, chamar o fillform, passando o retorno da chamada como parametro;
     //No final da requisição, setar loading para false
   }
+  // private findByIdUser(): CreateUser {
+  //   this.
+  // }
 
-  private fillForm(user: CreateUser): void {
+  //Preenche os campos do Profile
+  private fillForm(user: EditUser): void {
     this.form.patchValue({
       nome: user.nome,
       data_nasc: user.data_nasc,
@@ -115,7 +112,7 @@ export class UserComponent implements OnInit {
   }
 
   private editUser(): void {
-    const body = this.createOrUpdateUserPayload();
+    const body = this.updateUserPayload();
 
     this.userService.update(body).subscribe({
       next: () => {
@@ -134,4 +131,5 @@ export class UserComponent implements OnInit {
       },
     });
   }
+
 }
