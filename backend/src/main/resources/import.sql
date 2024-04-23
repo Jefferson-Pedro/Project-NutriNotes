a243513c20d4a9044f215c5df97fd2a734f87ab3
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-04-22 22:28:09 -0300

    Update import.sql

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,14 +1,6 @@
-insert into profile (id_profile, nome, data_nasc,sexo, email) values (100, 'Jefferson Pedro', '1993-05-25', 'M', 'exemple@exemple.com');
-insert into profile (id_profile, nome, data_nasc,sexo, email) values (101, 'Anna Thays', '2000-01-01', 'F', 'exemple@exemple.com');
-insert into profile (id_profile, nome, data_nasc,sexo, email) values (102, 'Pamela Cabral', '2000-01-01', 'F', 'exemple@exemple.com');
-insert into profile (id_profile, nome, data_nasc,sexo, email) values (103, 'Eduardo Correia', '2000-01-01', 'M', 'exemple@exemple.com');
-insert into profile (id_profile, nome, data_nasc,sexo, email) values (104, 'Anakin', '2000-01-01', 'M', 'exemple@exemple.com');
-insert into profile (id_profile, nome, data_nasc,sexo, email) values (105, 'Mestre Yoda', '2000-01-01', 'M', 'exemple@exemple.com');
+/* Inserindo os usuários*/
+INSERT INTO users (nome, data_nasc, sexo, email, senha, telefone, crn) 
+VALUES ('Anna Thays', '1990-01-01', 'F', 'anna.nutri@example.com', '123456', '123456789', 'CRN001');
 
-
-insert into business (id_business, nome, cnpj, responsavel_tec) values (1, 'Empresa Teste1', '0000000', 100);
-insert into business (id_business, nome, cnpj, responsavel_tec) values (2, 'Empresa Teste2', '0000000', 101);
-insert into business (id_business, nome, cnpj, responsavel_tec) values (3, 'Empresa Teste3', '0000000', 101);
-insert into business (id_business, nome, cnpj, responsavel_tec) values (4, 'Empresa Teste4', '0000000', 100);
-insert into business (id_business, nome, cnpj, responsavel_tec) values (5, 'Empresa Teste5', '0000000', 101);
-
+INSERT INTO users (nome, data_nasc, sexo, email, senha, telefone, crn) 
+VALUES ('Nathalia Martins', '1995-05-05', 'F', 'nathalia.nutri@example.com', '123456', '987654321', 'CRN002');
