54981e7c40ae025810f432310e88f47d37606c8e
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-04-23 17:55:20 -0300

    Update auth.service.ts

--- frontend/src/app/features/login-module/services/auth/auth.service.ts
+++ frontend/src/app/features/login-module/services/auth/auth.service.ts
@@ -10,18 +10,18 @@ import { environment } from 'src/environments/environment.development';
   providedIn: 'root'
 })
 export class AuthService {
- 
+
   private authUser: boolean = false;
 
   public emitter = new EventEmitter<boolean>();
   private route = inject(Router);
   private http = inject(HttpClient);
 
-  constructor() { } 
+  constructor() { }
 
   public loginValidation(login:Login): Observable<LoginUserResponse>{
     const url = `${environment.baseUrl}/login`;
-    
+
     return this.http.post<LoginUserResponse>(url, login).pipe(delay(1500));
   }
 
