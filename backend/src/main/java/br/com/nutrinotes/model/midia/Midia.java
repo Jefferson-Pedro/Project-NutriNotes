fe2257b6616f2f5cdb295c8f856a46fa1f36ad54
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-04-23 22:41:43 -0300

    Create Midia.java
    - Criação da classe modelo Midia, devidamente ligada a classe User;

---
+++ backend/src/main/java/br/com/nutrinotes/model/midia/Midia.java
@@ -0,0 +1,76 @@
+package br.com.nutrinotes.model.midia;
+
+import br.com.nutrinotes.model.user.User;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+
+@Entity
+@Table(name = "tbl_midia")
+public class Midia {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_midia")	
+	private Integer idMidia;
+	
+	@Column(name = "descricao", length = 255)
+	private String  descricao;
+	
+	@NotBlank
+	@NotEmpty
+	@Column(name = "link_midia", length = 255)
+	private String  linkMidia;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name="id_profile", referencedColumnName = "id_user")
+	private User user;
+	
+	// GET E SET
+
+	public Integer getIdMidia() {
+		return idMidia;
+	}
+
+	public void setIdMidia(Integer idMidia) {
+		this.idMidia = idMidia;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getLinkMidia() {
+		return linkMidia;
+	}
+
+	public void setLinkMidia(String linkMidia) {
+		this.linkMidia = linkMidia;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	@Override
+	public String toString() {
+		return "Midia [idMidia=" + idMidia + ", descricao=" + descricao + ", linkMidia=" + linkMidia + ", user=" + user
+				+ "]";
+	}
+}
