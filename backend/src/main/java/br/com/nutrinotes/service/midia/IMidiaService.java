00321eeb96d446166e7becddea5f5d0b8bff3878
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-04-23 22:42:34 -0300

    Create IMidiaService.java
    - Criação da interface Midia com alguns metodos CRUD;

---
+++ backend/src/main/java/br/com/nutrinotes/service/midia/IMidiaService.java
@@ -0,0 +1,10 @@
+package br.com.nutrinotes.service.midia;
+
+import br.com.nutrinotes.model.midia.Midia;
+
+public interface IMidiaService {
+	
+	public Midia create(Midia midia);
+	public boolean update(Midia midia);
+	public boolean delete(Integer id);
+}
