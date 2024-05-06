d76a03fe88eaf051943246ef28f7c62ac77f3d9d
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-05-05 23:06:30 -0300

    Update IMidiaService.java

--- backend/src/main/java/br/com/nutrinotes/service/midia/IMidiaService.java
+++ backend/src/main/java/br/com/nutrinotes/service/midia/IMidiaService.java
@@ -5,6 +5,7 @@ import br.com.nutrinotes.model.midia.Midia;
 public interface IMidiaService {
 	
 	public Midia create(Midia midia);
+	public Midia create(String linkMidia, Integer idMidia);
 	public boolean update(Midia midia);
 	public boolean delete(Integer id);
 	public Midia findById(Integer id);
