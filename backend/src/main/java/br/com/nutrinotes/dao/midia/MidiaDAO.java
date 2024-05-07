b6777de86d876e758aa6d8cc72c5be91fa5ba2b9
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-05-06 23:07:55 -0300

    Delete MidiaDAO.java

--- backend/src/main/java/br/com/nutrinotes/dao/midia/MidiaDAO.java
+++
@@ -1,9 +0,0 @@
-package br.com.nutrinotes.dao.midia;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.nutrinotes.model.midia.Midia;
-
-public interface MidiaDAO extends JpaRepository<Midia, Integer>{
-
-}
