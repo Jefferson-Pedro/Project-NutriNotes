f8585d1a14773453c7c4514b88292d0265ba6fd3
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-05-11 00:05:17 -0300

    Update TransferController.java
    - Melhora da logica para chamar o serviço de upload e setar o caminho da imagem no metodo update do user controller

--- backend/src/main/java/br/com/nutrinotes/controller/TransferController.java
+++ backend/src/main/java/br/com/nutrinotes/controller/TransferController.java
@@ -9,7 +9,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import br.com.nutrinotes.dto.UserEditDTO;
+import br.com.nutrinotes.model.user.User;
 import br.com.nutrinotes.service.media.IUploadService;
+import br.com.nutrinotes.service.user.IUser;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 
 @RestController
@@ -20,17 +26,24 @@ public class TransferController {
 	@Autowired 
 	IUploadService sevice;
 	
-	@PostMapping("/upload")
-	public ResponseEntity<?> upload(@RequestParam(name="file") MultipartFile file,  
-									@RequestParam(name="id") Integer id){
-		
-		String res = sevice.upload(file);
-		
-		if (res != null) {
-			//System.out.println("Armazenado em: " + res);
+	@Autowired
+	private IUser user_service;
 	
-			return ResponseEntity.status(201).body(res);
+	@PostMapping("/upload")
+	public ResponseEntity<?> upload(@RequestParam(name="file") @Valid MultipartFile file,  
+									@RequestParam(name="id") @NotNull @Positive Integer id){
+		
+		UserEditDTO user = user_service.findByIdForUpdate(id);
+		String res = sevice.upload(file);
+		if (res != null && user != null) {
+			//System.out.println("Armazenado em: " + res);
+			user.setLink_photo(res);
+			
+			if (user_service.update(user, id)) {
+				return ResponseEntity.status(201).body(res);
+			}
+			return ResponseEntity.status(400).body("Erro ao atualizar o usuário");
 		}
-		return ResponseEntity.badRequest().build();
+		return ResponseEntity.status(400).body("Erro ao processar a imagem!");
 	}
 }
