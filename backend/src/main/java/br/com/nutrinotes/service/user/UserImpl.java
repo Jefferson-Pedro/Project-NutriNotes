365570b3378f6d7461a44ff874b81f9e66278f3f
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-05-11 00:04:09 -0300

    Update UserImpl.java
    - Melhora na regra de negocios do metodo update que adiciona uma camada de segurança extra para verificar se as senha antiga confere antes de trocar para a senha nova;

--- backend/src/main/java/br/com/nutrinotes/service/user/UserImpl.java
+++ backend/src/main/java/br/com/nutrinotes/service/user/UserImpl.java
@@ -3,16 +3,14 @@ package br.com.nutrinotes.service.user;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
-
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
-
 import br.com.nutrinotes.dao.user.UserDAO;
-import br.com.nutrinotes.dto.UserEditFormDTO;
-import br.com.nutrinotes.dto.UserWithoutBusinessDTO;
+import br.com.nutrinotes.dto.UserEditDTO;
+import br.com.nutrinotes.dto.UserViewDTO;
 import br.com.nutrinotes.exception.InvalidAccountException;
 import br.com.nutrinotes.exception.UserException;
 import br.com.nutrinotes.model.user.User;
@@ -39,62 +37,82 @@ public class UserImpl implements IUser {
 	}
 
 	@Override
-	public boolean update(@Valid @NotNull User user, @NotNull @Positive Integer id) {
+	public boolean update(@Valid @NotNull UserEditDTO user, @NotNull @Positive Integer id) {
 		Optional<User> res = dao.findById(id);
 
 		if (res.isPresent()) {
 			User existingUser = res.get();
-			BCryptPasswordEncoder verifyOrEncoderPass = new BCryptPasswordEncoder();
 			
+			if(user.getSenha() != null && user.getNovaSenha() != null &&
+			  !user.getSenha().isEmpty() && !user.getNovaSenha().isEmpty()) {
+				
+				if(user.getNovaSenha().length() < 6 || user.getNovaSenha().length() > 155) {
+					
+					throw new InvalidAccountException("Nova senha deve ter entre 3 e 155 caracteres.");
+				}
+				
+				BCryptPasswordEncoder verifyOrEncoderPass = new BCryptPasswordEncoder();
+				
+				if (verifyOrEncoderPass.matches(user.getSenha(), existingUser.getSenha())) {
+					String newPassword = verifyOrEncoderPass.encode(user.getNovaSenha());
+					user.setSenha(newPassword);
+					
+				}else {
+					throw new InvalidAccountException("Senha incorreta. Verifique as informações e tente novamente! ");
+				}	
 			
-			if(verifyOrEncoderPass.matches(user.getSenha(), existingUser.getSenha())) {
-				String newPassword = verifyOrEncoderPass.encode(user.getSenha());
-				user.setSenha(newPassword);
-
-				BeanUtils.copyProperties(user, existingUser, "idUser");
+			} else {
+				user.setSenha(user.getSenha());
+				BeanUtils.copyProperties(user, existingUser, "idUser", "novaSenha");
 				dao.save(existingUser);
 				System.out.println("Usuário atualizado com sucesso!");
-				return true;
-
+				return true;	
 			}
-			
-			throw new InvalidAccountException("Senha incorreta. Verifique as informações e tente novamente! ");			
 		}
 		
 		throw new UserException("Usuário não existe no banco de dados!");
 	}
 
 	@Override
-	public List<UserWithoutBusinessDTO> findAll() {
+	public List<UserViewDTO> findAll() {
 		List<User> listUsers = dao.findAll();
-		List<UserWithoutBusinessDTO> listUsersDtos = listUsers.stream()
-				.map(UserWithoutBusinessDTO :: fromUserWithoutBusinessDTO)
+		List<UserViewDTO> listUsersDtos = listUsers.stream()
+				.map(UserViewDTO :: fromUserWithoutBusinessDTO)
 				.collect(Collectors.toList());
 		
 		return listUsersDtos;
 	}
 
 	@Override
-	public List<UserWithoutBusinessDTO> findByName(@NotNull @NotBlank String nome) {
+	public List<UserViewDTO> findByName(@NotNull @NotBlank String nome) {
 		List<User> listUsers = dao.findByNomeContaining(nome);
-		List<UserWithoutBusinessDTO> listUsersDtos = listUsers.stream()
-				.map(UserWithoutBusinessDTO :: fromUserWithoutBusinessDTO)
+		List<UserViewDTO> listUsersDtos = listUsers.stream()
+				.map(UserViewDTO :: fromUserWithoutBusinessDTO)
 				.collect(Collectors.toList());
 		
 		return listUsersDtos;
 	}
 
 	@Override
-	public UserEditFormDTO findById(@NotNull @Positive Integer id) {
+	public UserViewDTO findById(@NotNull @Positive Integer id) {
 		
 		User existingUser = dao.findById(id).orElse(null);
-		UserEditFormDTO userEditFormDTO = new UserEditFormDTO();
-		BeanUtils.copyProperties(existingUser, userEditFormDTO, "senha");
+		UserViewDTO userViewDTO = new UserViewDTO();
+		BeanUtils.copyProperties(existingUser, userViewDTO, "senha");
 		
-		if (userEditFormDTO != null) {
-			return userEditFormDTO;
+		if (userViewDTO != null) {
+			return userViewDTO;
 		}
 			throw new UserException("Usuário não encontrado!");	
+	}
+	
+	@Override
+	public UserEditDTO findByIdForUpdate(@NotNull @Positive Integer id) {
+		User existingUser = dao.findById(id).orElse(null);
+		UserEditDTO updateUser = new UserEditDTO();
+		BeanUtils.copyProperties(existingUser, updateUser, "novaSenha");
+		
+		return updateUser != null ? updateUser: null;
 	}
 
 	@Override
@@ -108,4 +126,6 @@ public class UserImpl implements IUser {
 		System.out.println("Ocorreu um erro ao excluir o perfil " + id);
 		return false;
 	}
+
+	
 }
