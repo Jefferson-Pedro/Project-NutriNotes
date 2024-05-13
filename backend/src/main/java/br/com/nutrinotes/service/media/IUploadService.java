61a0cd5825054031589e2622819d4a396c848dcd
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-05-12 21:19:41 -0300

    Update IUploadService.java

--- backend/src/main/java/br/com/nutrinotes/service/media/IUploadService.java
+++ backend/src/main/java/br/com/nutrinotes/service/media/IUploadService.java
@@ -1,5 +1,6 @@
 package br.com.nutrinotes.service.media;
 
+import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface IUploadService {
@@ -8,6 +9,6 @@ public interface IUploadService {
 	
 	public Boolean delete(String fileName);
 	
-	public String getImage(String imageName);
+	public Resource download(String imageName);
 
 }
