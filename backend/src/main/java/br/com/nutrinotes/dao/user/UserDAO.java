1b3ae9555f7c689288122168ae0d9bd2e44992bc
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-05-20 23:42:14 -0300

    Criação do metodo para retornar o usuário vinculado a tal imagem

--- backend/src/main/java/br/com/nutrinotes/dao/user/UserDAO.java
+++ backend/src/main/java/br/com/nutrinotes/dao/user/UserDAO.java
@@ -10,5 +10,8 @@ import br.com.nutrinotes.model.user.User;
 public interface UserDAO extends JpaRepository<User, Integer>{
 	
 	public User findByEmail(String email);
+	
 	public List<User> findByNomeContaining(String palavraChave);
+	
+	public User findByImageProfile(String nameImage);
 }
