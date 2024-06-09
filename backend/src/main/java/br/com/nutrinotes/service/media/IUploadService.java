0523eabceae22d281388e4cb2cc90f97e8f58da7
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-06-09 20:57:11 -0300

    Update IUploadService.java
    - Criação dos metodos de Download e Upload de arquivos na interface;

--- backend/src/main/java/br/com/nutrinotes/service/media/IUploadService.java
+++ backend/src/main/java/br/com/nutrinotes/service/media/IUploadService.java
@@ -2,16 +2,19 @@ package br.com.nutrinotes.service.media;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.Path;
 
 import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 public interface IUploadService {
-	
-	public String upload(MultipartFile file, String username);
 	
 	public Boolean delete(String fileName);
 	
-	public Resource download(String imageName) throws FileNotFoundException, IOException;
+	public Resource download(String file) throws FileNotFoundException, IOException;
+
+	public String upload(MultipartFile file, Integer id) throws FileNotFoundException;
 
 }
