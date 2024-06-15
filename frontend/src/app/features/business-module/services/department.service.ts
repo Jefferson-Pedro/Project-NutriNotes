cd680efb23b87756b08645e9cf5e749eb5a4b53f
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-06-15 10:30:47 -0300

    Create department.service.ts

---
+++ frontend/src/app/features/business-module/services/department.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Department } from 'src/app/core/models/Department';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DepartmentService {
+
+  constructor() { }
+
+  // public create(): Observable<Department>{
+
+  // }
+}
