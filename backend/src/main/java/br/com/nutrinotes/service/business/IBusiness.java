664f5b427162c1da25a901a824590d8eddf2b772
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-06-17 21:44:48 -0300

    Update IBusiness.java
    - Mudança no retorno do metodo delete para void e no metodo Update para objeto atualizado;

--- backend/src/main/java/br/com/nutrinotes/service/business/IBusiness.java
+++ backend/src/main/java/br/com/nutrinotes/service/business/IBusiness.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import br.com.nutrinotes.dto.BusinessDTO;
+import br.com.nutrinotes.dto.UserViewDTO;
 import br.com.nutrinotes.model.business.Business;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
@@ -15,10 +16,10 @@ import jakarta.validation.constraints.Positive;
 public interface IBusiness {
 	
 	public Business create(@Valid @NotNull Business novo);
-	public boolean update(@Valid @NotNull Business business, @NotNull @Positive Integer id);
-	public Page<BusinessDTO> findAllPage(Pageable pageable);
+	public Business update(@Valid @NotNull Business business, @NotNull @Positive Integer id);
+	public Page<BusinessDTO> findAllPageByUser(Pageable pageable, Integer id);
 	public List<BusinessDTO> findAll();
 	public List<BusinessDTO> findByName(@NotBlank @NotNull String nome);
 	public BusinessDTO findById(@NotNull @Positive  Integer id);
-	public boolean delete(@NotNull @Positive Integer id);
+	public void delete(@NotNull @Positive Integer id);
 }
