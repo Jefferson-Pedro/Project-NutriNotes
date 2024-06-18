4bfc60a1493b785afc0409515ef6c0c7e4b0d717
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-06-17 21:39:03 -0300

    Update FileSystemException.java
    - Inserção de serial Version

--- backend/src/main/java/br/com/nutrinotes/exception/FileSystemException.java
+++ backend/src/main/java/br/com/nutrinotes/exception/FileSystemException.java
@@ -1,7 +1,9 @@
 package br.com.nutrinotes.exception;
 
 public class FileSystemException extends RuntimeException{
-	
+
+	private static final long serialVersionUID = 1L;
+
 	public FileSystemException(String message) {
 		super(message);
 	}
