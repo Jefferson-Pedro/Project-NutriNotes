4ea82a7203fe8e74f755af180a7b3a1fc4263c60
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-06-17 21:39:18 -0300

    Update InvalidAccountException.java
    - Inserção de serial Version

--- backend/src/main/java/br/com/nutrinotes/exception/InvalidAccountException.java
+++ backend/src/main/java/br/com/nutrinotes/exception/InvalidAccountException.java
@@ -2,6 +2,8 @@ package br.com.nutrinotes.exception;
 
 public class InvalidAccountException extends RuntimeException{
 	
+	private static final long serialVersionUID = 1L;
+
 	public InvalidAccountException(String message) {
 		super(message);
 	} 
